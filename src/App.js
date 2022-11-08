2e86813d2cb19959b262e88fca655e28fa9456c5
inzamam nur <inzamamnur.14@gmail.com>
2022-11-08 11:47:09 +0600

    uninstall react bootsrtap setup tailwind and daisy ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { routes } from './Routes/Routes';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
