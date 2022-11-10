334a5c3b388065603671be912dcf7ab5ddc69483
inzamam nur <inzamamnur.14@gmail.com>
2022-11-10 10:31:27 +0600

    delete button setup

--- src/Pages/Review/Myreview.js
+++ src/Pages/Review/Myreview.js
@@ -6,19 +6,39 @@ import Reviewcard from "./Reviewcard";
 
 const Myreview = () => {
   const { user } = useContext(AuthContext);
-  const [reviews, setreview] = useState([]);
+  const [reviews, setreviews] = useState([]);
   useEffect(() => {
     fetch(`http://localhost:5000/reviews?email=${user?.email}`)
       .then((res) => res.json())
-      .then((data) => setreview(data));
+      .then((data) => setreviews(data));
   }, [user?.email]);
+
+//delete review 
+  const handleDelete = id =>{
+    console.log('clicked')
+    const proceed = window.confirm('Are you sure, you want to cancel this order');
+    if(proceed){
+        fetch(`http://localhost:5000/reviews/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.deletedCount > 0){
+                alert('deleted successfully');
+                const remaining = reviews.filter(rev => rev._id !== id);
+                setreviews(remaining);
+            }
+        })
+    }
+}
   return (
     <div>
       <h2>My review {reviews.length}</h2>
 
       <div className='lg:grid grid-cols-5 gap-4 mt-5 mb-5 '>
      {reviews.map((review) => (
-        <Reviewcard key={review._id} review={review}></Reviewcard>
+        <Reviewcard key={review._id} review={review} handleDelete={handleDelete}></Reviewcard>
       ))}
      </div>
     </div>
